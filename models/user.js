e3d69486676571b8eacd851953b0572f28b7a814
Vladislav <vladdevs@yandex.ru>
2023-06-18 22:06:03 +0300

    feat: auth

--- models/user.js
+++ models/user.js
@@ -41,6 +41,7 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+    select: false,
     minlength: 8,
   },
 });
@@ -48,6 +49,7 @@ const userSchema = new mongoose.Schema({
 // eslint-disable-next-line func-names
 userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email })
+    .select('+password')
     .then((user) => {
       if (!user) {
         return Promise.reject(new Error('Неправильные почта или пароль'));
