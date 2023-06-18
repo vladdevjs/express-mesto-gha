03cb5bc8403a131ae53acd7b8b14cdf4a6c42d70
Vladislav <vladdevs@yandex.ru>
2023-06-18 19:56:14 +0300

    feat: auth

--- models/user.js
+++ models/user.js
@@ -25,12 +25,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: defaultAvatar,
     validate: {
-      validator(v) {
-        // eslint-disable-next-line no-useless-escape
-        return /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/.test(
-          v,
-        );
-      },
+      validator: (v) => validator.isURL(v),
       message: 'Некорректный формат ссылки на аватар',
     },
   },
