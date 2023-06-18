84c590438cb2fced57723ba6035e1c4c954ff3c1
Vladislav <vladdevs@yandex.ru>
2023-06-19 00:24:47 +0300

    feat: auth

--- controllers/users.js
+++ controllers/users.js
@@ -88,7 +88,6 @@ const createUser = (req, res, next) => {
 const login = (req, res, next) => {
   const { email, password } = req.body;
 
-
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, 'secret-key', {
