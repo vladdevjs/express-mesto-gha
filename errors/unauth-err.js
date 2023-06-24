d0695ee9a5112377e9151e0d5ba2ab193a3e6d72
Vladislav <vladdevs@yandex.ru>
2023-06-24 21:33:23 +0300

    refactor

--- errors/unauth-err.js
+++ errors/unauth-err.js
@@ -3,7 +3,7 @@ const httpConstants = require('http2').constants;
 class UnAuthError extends Error {
   constructor(message) {
     super(message);
-    this.statusCode = httpConstants.HTTP_STATUS_DENIED;
+    this.statusCode = 401;
   }
 }
 
