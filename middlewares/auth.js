da86f347dde97333476048f852003f58f68bf80b
Vladislav <vladdevs@yandex.ru>
2023-06-27 09:45:58 +0300

    Refactoring

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 const { secretKey } = require('../config');
 const UnAuthError = require('../errors/unauth-err');
 
+// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { jwt: token } = req.cookies;
 
